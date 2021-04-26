af5600abe3ca0ee7deb59e0a02ff83e0709f10ba
rhaoddl72 <rhaoddl72@naver.com>
2021-04-26 17:34:27 +0900

    자바 1일차 - 주석 설명 예제

---
+++ java_kms/src/day1/HelloWorld.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//주석
+		/* 작성일 : 2021-04-26
+		 * 작성자 : 고민석
+		 * 내용 : 프로젝트 생성 및 테스트   */
+		
+		/* @author rhaod */
+		
+		
+		System.out.println("dont cleep 으아아아악!! 죽인다아아앙!");
+
+	}
+
+}
