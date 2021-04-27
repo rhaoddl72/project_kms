a24e9eae469114752d1edcc6dfa68ea076b7bc59
rhaoddl72 <rhaoddl72@naver.com>
2021-04-27 17:42:56 +0900

    자바 2일차 조건 선택 연산자 예제

---
+++ java_kms/src/day2/ConditionalOperator.java
@@ -0,0 +1,16 @@
+package day2;
+
+
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		int num = 6;
+		
+		String result = (num%2==0) ? "짝수" : "홀수" ;
+		
+		System.out.println(num + " 는 " + result);
+	}
+	
+
+}
