326e0782bb72776b3cc5a973600684005ec625e5
rhaoddl72 <rhaoddl72@naver.com>
2021-04-28 17:30:47 +0900

    자바 3일차 for문 1부터 5까지 출력예제

---
+++ java_kms/src/day3/For2.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		//1부터 5까지 출력하는 예제를 작성하시오.
+		
+	    for(int i = 1; i<=5; i++) {
+	    	System.out.println(i);
+	    	
+	    }
+	}
+}
