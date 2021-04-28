0951e1bc85de0c520691f7f6cd385dbe531abfba
rhaoddl72 <rhaoddl72@naver.com>
2021-04-28 17:31:07 +0900

    자바 3일차 for문 구구단 예제

---
+++ java_kms/src/day3/For3.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class For3 {
+	
+	public static void main(String[] args) {
+		
+		//구구단 7단을 출력하는 코드 작성
+		
+		for(int i = 1; i<=9; i++) {
+			
+			System.out.println("7"+" * "+i+" = "+(7*i));
+		}
+	}
+
+}
