d49a4a337c613edeeb4566c0bb38e184f7b4ee7d
rhaoddl72 <rhaoddl72@naver.com>
2021-04-28 17:31:57 +0900

    자바 3일차 for문 1부터 5까지 합구하기

---
+++ java_kms/src/day3/For5.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class For5 {
+
+	public static void main(String[] args) {
+		// 1부터 5까지의 합을 구하는 예제
+		
+		int sum = 0;
+		
+		for(int i=1; i<=5; i++) {
+			
+			sum += i;
+			
+			
+			
+		}
+		System.out.println("1부터 5까지의 합 : " + sum);
+		
+	}
+}
