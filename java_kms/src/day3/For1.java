a31786f726602e6502a0a7095f09552d54de15b0
rhaoddl72 <rhaoddl72@naver.com>
2021-04-28 17:30:06 +0900

    자바 3일차 기본 for문

---
+++ java_kms/src/day3/For1.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class For1 {
+	
+	public static void main(String[] args) {
+		// Hello world 5번 반복출력
+		
+		for(int i = 1; i<=5; i++) {
+			System.out.println("Hello world");
+		}
+	}
+
+}
