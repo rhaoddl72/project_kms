959136d35fbb1993a37933a2dd5c992347cd96a1
rhaoddl72 <rhaoddl72@naver.com>
2021-04-28 17:33:38 +0900

    자바 3일차 switch문 문자열 출력 예제

---
+++ java_kms/src/day3/Switch2.java
@@ -0,0 +1,39 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Switch2 {
+	
+	public static void main(String[] args) {
+		// 계절을 영어로 입력받아 입력받은 계절을 한글로 출력하는 예제
+		// 예시 : 계절을 영어로 입력하세요 : Spring == 봄입니다.
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("계절을 영어로 입력하세요 :");
+		String season = sc.next();
+		
+		switch(season) {
+		
+		case  "spring" :
+			System.out.println("봄입니다.");
+			break;
+		case "summer" :
+			System.out.println("여름입니다.");
+			break;
+		case "fall" :
+			System.out.println("가을입니다.");
+			break;
+		case "winter" :
+			System.out.println("겨울입니다.");
+		    break;
+		default :
+			System.out.println("잘못 입력했습니다.");
+		
+		
+		}
+		
+		sc.close();
+		
+	}
+
+}
