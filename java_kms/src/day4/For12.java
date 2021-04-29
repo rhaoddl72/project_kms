0e542c0eaf7a34715d2e67a7c72360ef9470b34d
rhaoddl72 <rhaoddl72@naver.com>
2021-04-29 17:34:28 +0900

    자바 4일차 2중 for문 예제

---
+++ java_kms/src/day4/For12.java
@@ -0,0 +1,30 @@
+package day4;
+
+
+
+public class For12 {
+	
+	public static void main(String[] args) {
+		/*
+		 * 하나의 *를 이용하여 다음과 같이 출력
+		 * *****
+		 * *****
+		 * *****
+		 * *****
+		 * *****
+		 * */
+		
+		for(int j=1; j <=5 ; j++) {
+			for(int i = 1; i<=5; i++) {
+				
+				System.out.print("*");
+			}	
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+
+}
