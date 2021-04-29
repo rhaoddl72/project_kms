59e86446ab62ed98c8f0b97e4c183a4a414f39f1
rhaoddl72 <rhaoddl72@naver.com>
2021-04-29 13:07:43 +0900

    자바 4일차 무한루프

---
+++ java_kms/src/day4/For9.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class For9 {
+	
+	public static void main(String[] args) {
+		// 무한루프 : 반복문이 끝나지 않는 경우를 무한루프라 한다.
+		
+		
+		for(;;) {
+			
+			System.out.println("Hello World");
+			
+		}
+	}
+
+}
