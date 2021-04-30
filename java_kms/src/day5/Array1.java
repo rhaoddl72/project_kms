e0f1af8261e0b2367cda3fbd1fad31fb10bfe33c
rhaoddl72 <rhaoddl72@naver.com>
2021-04-30 17:59:53 +0900

    배열 선언 예제

---
+++ java_kms/src/day5/Array1.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class Array1 {
+	
+	public static void main(String[] args) {
+		int[] arr1;
+		
+		int[] arr2;
+		int[] arr3 = new int[5];
+		int[] arr4 = {1,2,3,4};
+		int[] arr5 = new int[] {1,2,3,4};
+		
+		int[] arr9, arr10; // 둘다 배열
+		int arr11[], arr12; // arr11만 배열
+		
+		System.out.println(arr3[0]);
+		}
+
+}
