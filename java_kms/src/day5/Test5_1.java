e6d17c912e35fd77295f28b3ae1c1ef7b5ca5ff7
rhaoddl72 <rhaoddl72@naver.com>
2021-04-30 18:03:17 +0900

    while문 예제

---
+++ java_kms/src/day5/Test5_1.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class Test5_1 {
+	
+	public static void main(String[] args) {
+		/*
+		 * while문 이용하여 구구단 7단 출력 코
+		 * */
+		
+		int i = 1;
+		
+		while(i<=9) {
+			System.out.println("7"+ " x " + i + " = " + (7*i));
+			i++;
+		}
+		
+	}
+
+}
