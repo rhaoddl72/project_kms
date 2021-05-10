cfc0b86c456720bcf8ea6aae9a50bb394f65fa33
project_KMS <rhaoddl72@naver.com>
2021-05-10 17:41:02 +0900

    Create D.java

---
+++ java_kms/src/day10/D.java
@@ -0,0 +1,16 @@
+package day10;
+
+public class D {
+
+	private int num; //private 접근제한자 본인 클래스의 메소드에서만 사용 가능
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+}
