0c311840fd6702ee380fa1663ad3016a1884de5c
project_KMS <rhaoddl72@naver.com>
2021-05-11 17:48:25 +0900

    Board 클래스 활용 예제

---
+++ java_kms/src/day11/BoardTest.java
@@ -0,0 +1,11 @@
+package day11;
+
+public class BoardTest {
+
+	public static void main(String[] args) {
+		
+		Board board = new Board("제목입니다.", "내용입니다.", "고민석", "2021-05-11", "공지", 1);
+		
+		board.print();
+	}
+}
