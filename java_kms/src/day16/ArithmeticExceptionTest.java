1ca3be4b19b20e8ac784746995f6948e350b0f6b
project_KMS <rhaoddl72@naver.com>
2021-05-18 17:40:58 +0900

    실행 예외 예제

---
+++ java_kms/src/day16/ArithmeticExceptionTest.java
@@ -0,0 +1,13 @@
+package day16;
+
+public class ArithmeticExceptionTest {
+
+	
+	public static void main(String[] args) {
+		// 정수/정수에서 0으로 나눌때 발생
+		
+		int num1 = 1;
+		int num2 = 0;
+		System.out.println(num1/num2);
+	}
+}
