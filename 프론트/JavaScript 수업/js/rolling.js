94283f6af2a8b3919112835f9685f72d58d0fee4
rhaoddl72 <rhaoddl72@naver.com>
2021-06-25 18:05:55 +0900

    롤링 함수 모아놓은 js파일

--- 프론트/JavaScript 수업/js/rolling.js
+++ 프론트/JavaScript 수업/js/rolling.js
@@ -1,3 +1,23 @@
+
+//롤링이라는 메소드 만들기
+// 바텀은 따로 연습해보기
+function rolling(direction, list, listItem, margin, animateTime, intervalTime){
+    var id;
+
+    //소문자랑 비교해준다??
+    if(direction.toLowerCase() == 'left') {
+        return rollingLeft(list, listItem, animateTime, intervalTime);
+    }
+    else if(direction.toLowerCase() == 'right'){
+        return rollingRight(list, listItem, animateTime, intervalTime);
+    }
+    else{
+        return rollingTop(list, listItem, margin, animateTime, intervalTime);
+    }
+ }
+
+
+
 function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
     var id = setInterval(function(){
         // list와 listItem 사이에 공백을 넣는 이유는 부모 자식임을 표현하기 위해서
@@ -16,12 +36,25 @@ function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
  function rollingLeft(list, listItem, animateTime, intervalTime){
 
     var id = setInterval(function(){
-        if($(list+ ' ' + listItem).first()){
+        if(!$(list+ ' ' + listItem).first().is(':animated')){
         var width =  $(list+ ' ' + listItem).first().width();
         $(list+ ' ' + listItem).first().animate({'margin-left':'-'+width},animateTime,function(){
             $(this).detach().appendTo(list).removeAttr('style');
         });
         }
     },intervalTime)
+    return id;
+}
+
+function rollingRight(list, listItem, animateTime, intervalTime){
+    var id = setInterval(function(){
+        if(!$(list + ' ' + listItem).first().is(':animated')){
+        var width = $(list + ' ' + listItem).last().width();
+        /*width숫자로 찍혀서 ''없이 그냥- 붙여도 된다.*/
+        $(list + ' ' + listItem).last().detach().prependTo(list).css('margin-left',-width+'px')
+        .animate({'margin-left':'0px'},animateTime);
+        }
+    },intervalTime);
+    
     return id;
 }
