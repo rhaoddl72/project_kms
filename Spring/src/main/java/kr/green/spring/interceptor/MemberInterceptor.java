76bf5ede1832667eb2f51cc9b0f9a3a273a7e3fb
rhaoddl72 <rhaoddl72@naver.com>
2021-07-13 10:22:56 +0900

    Spring : 사용자에 따른 페이지 접근 권한 설정

---
+++ Spring/src/main/java/kr/green/spring/interceptor/MemberInterceptor.java
@@ -0,0 +1,31 @@
+package kr.green.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MemberInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler)
+			throws Exception {
+//		세션에있는 유저정보 가져옴
+//		아이디가 뭔지 비번이 뭔지 정보를 알려는게 아니고 유저정보가 있는지 없는지만 판단
+//		그래서 딱히 형변환하고 할 필요가 없다.
+//		세션에 user로 저장했기 때문에 getAttribute에 user라고 적음
+		HttpSession session = request.getSession();
+		Object user = session.getAttribute("user");
+		if(user == null) {
+			response.sendRedirect(request.getContextPath()+"/signin");
+//			false : 원래 가려던 경로를 못가게 한다.
+			return false;
+		}
+//		true : 원래 가려던 경로로 간다.
+		return true;
+	}
+
+}
