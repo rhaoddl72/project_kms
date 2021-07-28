04877dfdeb1c048ec8c790f822504e16fafbf819
rhaoddl72 <rhaoddl72@naver.com>
2021-07-28 11:01:04 +0900

    Test : lombok 추가 및 기본 세팅 완료

---
+++ test/src/main/java/kr/green/test/service/MemberServiceImp.java
@@ -0,0 +1,14 @@
+package kr.green.test.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.green.test.dao.MemberDAO;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class MemberServiceImp implements MemberService {
+	
+	MemberDAO memberDao;
+}
